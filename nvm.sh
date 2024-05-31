011e5a09155825900e897d63f7d67d8455ecd391
Sumair Syed <ssy@SVMBP15A.local>
2024-05-31 16:04:54 +1000

    Fixed yml cmds

--- nvm.sh
+++ nvm.sh
@@ -1,6 +1,5 @@
-#if [[ $(nvm ls | grep v18.19) == "" ]]; then
-#  nvm install v18.19
-#else
-#  nvm use v18.19
-#fi
-nvm install v18.19
+if [[ $(nvm ls | grep v18.19) == "" ]]; then
+  nvm install v18.19
+else
+  nvm use v18.19
+fi
