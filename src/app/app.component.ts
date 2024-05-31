4c1e7ca46576a5a54caff0a5d9390ad35c9d17b4
sumairahmedsyed <109499227+sumairahmedsyed@users.noreply.github.com>
2024-05-31 14:39:51 +1000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
+
+Amplify.configure(outputs);
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, TodosComponent],
+})
+export class AppComponent {
+  title = 'amplify-angular-template';
+}
