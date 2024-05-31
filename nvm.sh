6cd571219c56c1e85662f53f84e4c00378c8bf29
Sumair Syed <ssy@SVMBP15A.local>
2024-05-31 15:59:54 +1000

    Updated wrong node version

--- nvm.sh
+++ nvm.sh
@@ -1,6 +1,5 @@
-#if [[ $(nvm ls | grep v18.9) == "" ]]; then
-#  nvm install v18.9
-#else
-#  nvm use v18.9
-#fi
-nvm install v18.9
+if [[ $(nvm ls | grep v18.19) == "" ]]; then
+  nvm install v18.19
+else
+  nvm use v18.19
+fi
