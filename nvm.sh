022d8b844b2971e56ff45b27bb7aff3b237bd7fd
Sumair Syed <ssy@SVMBP15A.local>
2024-05-31 16:01:51 +1000

    Explicitely install node v18.19

--- nvm.sh
+++ nvm.sh
@@ -1,5 +1,6 @@
-if [[ $(nvm ls | grep v18.19) == "" ]]; then
-  nvm install v18.19
-else
-  nvm use v18.19
-fi
+#if [[ $(nvm ls | grep v18.19) == "" ]]; then
+#  nvm install v18.19
+#else
+#  nvm use v18.19
+#fi
+nvm install v18.19
